21b8658034e4979a849a5754d15b89a71b90751e
sourav-coder <souravsarkar020@gmail.com>
2020-07-12 10:13:01 +0530

    My Portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("btn-geeksgod-desc").addEventListener("click", geeksgod);
+
+function geeksgod() {
+    document.getElementById("btn-geeksgod-desc").innerHTML = "<br>Build a   new   Resume building website using HTML, CSS, JavaScript, Bootstrap, and jQuery, and used PHP is implemented for the database connectivity for the Login and Signup systems. ";
+}
+
+document.getElementById("btn-technocolabs-desc").addEventListener("click", technocolabs);
+
+function technocolabs() {
+    document.getElementById("btn-technocolabs-desc").innerHTML =
+        "<br>Learned to work on Python Data Structures<br>Accomplished assignments on time <br>Worked on python libraries, Git and GitHub.<br>Working On Data Science libraries";
+}
