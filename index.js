21aff22403b8ba1ed2fa6abe17b36bb38d7c248e
sourav-coder <souravsarkar020@gmail.com>
2020-07-15 00:08:04 +0530

    added new styles and other info

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 document.getElementById("btn-geeksgod-desc").addEventListener("click", geeksgod);
 
 function geeksgod() {
@@ -6,7 +7,31 @@ function geeksgod() {
 
 document.getElementById("btn-technocolabs-desc").addEventListener("click", technocolabs);
 
+
+
 function technocolabs() {
     document.getElementById("btn-technocolabs-desc").innerHTML =
         "<br>Learned to work on Python Data Structures<br>Accomplished assignments on time <br>Worked on python libraries, Git and GitHub.<br>Working On Data Science libraries";
 }
+
+
+
+
+
+// code not working ---- 
+
+// document.getElementsByClassName('navbar-toggler-icon')[0].addEventListener('click',navicon);
+
+// var x = document.getElementsByClassName('navbar-toggler-icon')[0];
+
+// function navicon(){
+//     console.log(x);
+    
+//     if (x.innerHTML==='&#x292C;'){
+
+//         x.innerHTML="&#9776;";
+//     }
+//     else{
+//         x.innerHTML="&#x292C;";
+//     }
+// }
